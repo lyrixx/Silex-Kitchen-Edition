c0a9aecb79a0b1ca5586264833e84c7dd44bac11
Grégoire Pineau <lyrixx@lyrixx.info>
2011-09-23 11:47:07 +0200

    Updated Silex vendor

--- src/app.php
+++ src/app.php
@@ -2,32 +2,32 @@
 
 require_once __DIR__ . '/../vendor/silex/autoload.php';
 
-use Silex\Extension\SessionExtension;
-use Silex\Extension\SymfonyBridgesExtension;
-use Silex\Extension\ValidatorExtension;
-use Silex\Extension\FormExtension;
-use Silex\Extension\UrlGeneratorExtension;
-use Silex\Extension\TranslationExtension;
-use Silex\Extension\TwigExtension;
-use Silex\Extension\DoctrineExtension;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\SymfonyBridgesServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
 
 $app = new Silex\Application();
 
 require __DIR__ . '/config.php';
 
-$app->register(new SessionExtension());
-$app->register(new SymfonyBridgesExtension());
-$app->register(new ValidatorExtension());
-$app->register(new FormExtension());
-$app->register(new UrlGeneratorExtension());
+$app->register(new SessionServiceProvider());
+$app->register(new SymfonyBridgesServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new FormServiceProvider());
+$app->register(new UrlGeneratorServiceProvider());
 
-$app->register(new TranslationExtension(), array(
+$app->register(new TranslationServiceProvider(), array(
     'locale_fallback' => $app['locale'],
 ));
 $app['translator.loader'] = new Symfony\Component\Translation\Loader\YamlFileLoader();
 
 
-$app->register(new TwigExtension(), array(
+$app->register(new TwigServiceProvider(), array(
     'twig.options'  => array('cache' => false, 'strict_variables' => true),
     'twig.path'     => array(
         __DIR__ . '/../views/common',
@@ -41,7 +41,7 @@ $app['twig.configure'] = $app->protect(function($twig) use ($oldTwigConfiguratio
     $twig->addExtension(new Twig_Extensions_Extension_Debug());
 });
 
-$app->register(new DoctrineExtension(), array(
+$app->register(new DoctrineServiceProvider(), array(
     'db.options'    => array(
         'driver'    => $app['db.config.driver'],
         'dbname'    => $app['db.config.dbname'],
