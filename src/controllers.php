ccb93cb660d18d664b6113a90623f4d907fc4e02
Grégoire Pineau <lyrixx@lyrixx.info>
2011-10-02 13:22:17 +0300

    Fixed typo

--- src/controllers.php
+++ src/controllers.php
@@ -81,6 +81,7 @@ $app->error(function (\Exception $e, $code) use ($app) {
     switch ($code) {
         case 404:
             $message = 'The requested page could not be found.';
+            break;
         default:
             $message = 'We are sorry, but something went terribly wrong.';
     }
