0fb8f2acba3beb74b96f4ed69fe6e2349fcc223d
umpirsky <umpirsky@gmail.com>
2012-10-12 15:12:12 +0200

    Moved resources to resources directory.

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -8,7 +8,7 @@ $app['translator.messages'] = array(
 );
 
 // Cache
-$app['cache.path'] = __DIR__ . '/../../cache';
+$app['cache.path'] = __DIR__ . '/../cache';
 
 // Http cache
 $app['http_cache.cache_dir'] = $app['cache.path'] . '/http';
