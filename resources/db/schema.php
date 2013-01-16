cf656f349fa2d10eff5cc1217b2ca165168f30f6
Juan Pastor <juantoleillo@gmail.com>
2013-01-16 19:00:30 +0100

    Add autoincrement on db schema post example.

--- resources/db/schema.php
+++ resources/db/schema.php
@@ -7,7 +7,7 @@
 $schema = new \Doctrine\DBAL\Schema\Schema();
 
 $post = $schema->createTable('post');
-$post->addColumn('id', 'integer', array('unsigned' => true));
+$post->addColumn('id', 'integer', array('unsigned' => true, 'autoincrement' => true));
 $post->addColumn('title', 'string', array('length' => 32));
 $post->setPrimaryKey(array('id'));
 
