fa159b739536ec621813905a09bae44915812b64
Grégoire Pineau <lyrixx@lyrixx.info>
2013-03-02 14:16:34 +0100

    Used twitter bootstrap less files

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -17,18 +17,18 @@ $app['http_cache.cache_dir'] = $app['cache.path'] . '/http';
 $app['twig.options.cache'] = $app['cache.path'] . '/twig';
 
 // Assetic
-$app['assetic.enabled'] = true;
-$app['assetic.path_to_cache']       = $app['cache.path'] . '/assetic' ;
-$app['assetic.path_to_web']         = __DIR__ . '/../../web/assets';
-$app['assetic.input.path_to_assets']    = __DIR__ . '/../assets/';
-$app['assetic.input.path_to_css']       = $app['assetic.input.path_to_assets'] . 'css/*.css';
+$app['assetic.enabled']              = true;
+$app['assetic.path_to_cache']        = $app['cache.path'] . '/assetic' ;
+$app['assetic.path_to_web']          = __DIR__ . '/../../web/assets';
+$app['assetic.input.path_to_assets'] = __DIR__ . '/../assets';
+
+$app['assetic.input.path_to_css']       = $app['assetic.input.path_to_assets'] . '/less/style.less';
 $app['assetic.output.path_to_css']      = 'css/styles.css';
 $app['assetic.input.path_to_js']        = array(
-    $app['assetic.input.path_to_assets'] . 'js/bootstrap.min.js',
-    $app['assetic.input.path_to_assets'] . 'js/script.js',
+    $app['assetic.input.path_to_assets'] . '/js/bootstrap.min.js',
+    $app['assetic.input.path_to_assets'] . '/js/script.js',
 );
 $app['assetic.output.path_to_js']       = 'js/scripts.js';
-$app['assetic.filter.yui_compressor.path'] = '/usr/share/yui-compressor/yui-compressor.jar';
 
 // Doctrine (db)
 $app['db.options'] = array(
--- src/app.php
+++ src/app.php
@@ -51,22 +51,13 @@ if (isset($app['assetic.enabled']) && $app['assetic.enabled']) {
             'auto_dump_assets' => $app['debug'],
         ),
         'assetic.filters' => $app->protect(function($fm) use ($app) {
-            $fm->set('yui_css', new Assetic\Filter\Yui\CssCompressorFilter(
-                $app['assetic.filter.yui_compressor.path']
-            ));
-            $fm->set('yui_js', new Assetic\Filter\Yui\JsCompressorFilter(
-                $app['assetic.filter.yui_compressor.path']
-            ));
+            $fm->set('lessphp', new Assetic\Filter\LessphpFilter());
         }),
         'assetic.assets' => $app->protect(function($am, $fm) use ($app) {
             $am->set('styles', new Assetic\Asset\AssetCache(
                 new Assetic\Asset\GlobAsset(
                     $app['assetic.input.path_to_css'],
-                    // Yui compressor is disabled by default.
-                    // If you need it, and you have installed it, uncomment the
-                    // next line, and delete "array()"
-                    //array($fm->get('yui_css'))
-                    array()
+                    array($fm->get('lessphp'))
                 ),
                 new Assetic\Cache\FilesystemCache($app['assetic.path_to_cache'])
             ));
@@ -74,12 +65,7 @@ if (isset($app['assetic.enabled']) && $app['assetic.enabled']) {
 
             $am->set('scripts', new Assetic\Asset\AssetCache(
                 new Assetic\Asset\GlobAsset(
-                    $app['assetic.input.path_to_js'],
-                    // Yui compressor is disabled by default.
-                    // If you need it, and you have installed it, uncomment the
-                    // next line, and delete "array()"
-                    //array($fm->get('yui_js'))
-                    array()
+                    $app['assetic.input.path_to_js']
                 ),
                 new Assetic\Cache\FilesystemCache($app['assetic.path_to_cache'])
             ));
