3ee68158c4c630b349596778b2a4455df64ecae7
Grégoire Pineau <lyrixx@lyrixx.info>
2015-12-13 23:57:37 +0100

    Bumped to php 5.5

--- src/App/Composer/Script.php
+++ src/App/Composer/Script.php
@@ -6,8 +6,8 @@ class Script
 {
     public static function install()
     {
-        mkdir('var/cache', 0777);
-        mkdir('var/log', 0777);
+        @mkdir('var/cache', 0777);
+        @mkdir('var/log', 0777);
         chmod('bin/console', 0755);
     }
 }
